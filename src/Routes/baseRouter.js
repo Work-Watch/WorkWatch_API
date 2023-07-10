dc9e7f17484b50d77563a269cf53fafbb3a817cf
noealas53 <00262020@uca.edu.sv>
2023-07-10 17:55:26 -0600

    upgrate route

--- src/Routes/baseRouter.js
+++ src/Routes/baseRouter.js
@@ -10,12 +10,12 @@ const passport = require('passport');
 
 const baseRouter = Router();
 
-baseRouter.use('/user', passport.authenticate("jwt", { session: false }), userRouter);
-baseRouter.use('/rol', passport.authenticate("jwt", { session: false }), rolRouter);
-baseRouter.use('/team', passport.authenticate("jwt", { session: false }), teamRouter);
-baseRouter.use('/task', passport.authenticate("jwt", { session: false }), taskRouter);
-baseRouter.use('/userteam', passport.authenticate("jwt", { session: false }), UserTeamRouter);
-baseRouter.use('/auth', authRouter);
-baseRouter.use('/verifiedCode', isVerifiedCode);
+baseRouter.use('/api/user', passport.authenticate("jwt", { session: false }), userRouter);
+baseRouter.use('/api/rol', passport.authenticate("jwt", { session: false }), rolRouter);
+baseRouter.use('/api/team', passport.authenticate("jwt", { session: false }), teamRouter);
+baseRouter.use('/api/task', passport.authenticate("jwt", { session: false }), taskRouter);
+baseRouter.use('/api/userteam', passport.authenticate("jwt", { session: false }), UserTeamRouter);
+baseRouter.use('/api/auth', authRouter);
+baseRouter.use('/api/verifiedCode', isVerifiedCode);
 
 module.exports = baseRouter;
